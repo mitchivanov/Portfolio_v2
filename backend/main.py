fcc34ed7d1069a526e6d97958004b6ed8cf54827
Michael Ivanov <hackermanmitch@gmail.com>
2025-02-10 00:01:55 +0300

    fuck you Doazhu

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=["http://doazhu.ru", "https://doazhu.ru"],  
     allow_methods=["*"],
     allow_headers=["*"],
-))
+)
 
 # Пример данных
 @app.get("/api/items")
