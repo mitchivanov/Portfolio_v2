1a97beb50b8435bf5649dc99a77a9b7a094dd32e
Doazhu <alexandrrenev07@gmail.com>
2025-02-09 23:31:08 +0300

    update
    dadadadadadada
    
    pipimm

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Настройка CORS для разрешения запросов с React
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # URL React-приложения
-    allow_credentials=True,
+    allow_origins=["http://doazhu.ru", "https://doazhu.ru"],  
     allow_methods=["*"],
     allow_headers=["*"],
-)
+))
 
 # Пример данных
 @app.get("/api/items")
